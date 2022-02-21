90a66362a5457adbe21813c9a6258199ffb5926b
diego <diego.maranges@intraway.com>
2022-02-21 15:44:42 -0300

    modify json file and add this elements to ObjAndAreas component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Activities from './Components/Activities/Activities';
+import ObjAndAreas from './Components/ObjAndAreas/ObjAndAreas';
 
 function App() {
-  return (
-    <div className="App">
-      <Activities></Activities>    
-    </div>
-  );
+    return (
+            <div className="container">
+                <ObjAndAreas>
+                </ObjAndAreas>
+            </div>
+    );
 }
 
 export default App;
--- src/Components/ObjAndAreas/ObjAndAreas.jsx
+++ src/Components/ObjAndAreas/ObjAndAreas.jsx
@@ -1,39 +1,111 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { obj } from '../../sources/Objectives';
 
-function ObjAndAreas(params) {
+export default function ObjAndAreas(params) {
+    const [areas, setAreas] = useState([])
+    const [row1, setRow1] = useState([])
+    const [row2, setRow2] = useState([])
+    const [row3, setRow3] = useState([])
+    const [row4, setRow4] = useState([])
+    const [row5, setRow5] = useState([])
+    const [row6, setRow6] = useState([])
+    const [row7, setRow7] = useState([])
+
+    useEffect(() => {
+        console.log(obj);
+        for (let i = 0; i < obj.AreaDeCrecimiento.length; i++) {
+            for (let j = 0; j < obj.ObjetivosEducativos.Unidad[i].length; j++) {
+                switch (j) {
+                    case 0:
+                        setAreas(areas.push(obj.ObjetivosEducativos.Unidad[i][j]));
+                        break;
+                    case 1:
+                        setRow1(row1.push(obj.ObjetivosEducativos.Unidad[i][j]));
+                        break;
+                    case 2:
+                        setRow2(row2.push(obj.ObjetivosEducativos.Unidad[i][j]));
+                        break;
+                    case 3:
+                        setRow3(row3.push(obj.ObjetivosEducativos.Unidad[i][j]));
+                        break;
+                    case 4:
+                        setRow4(row4.push(obj.ObjetivosEducativos.Unidad[i][j]));
+                        break;
+                    case 5:
+                        setRow5(row5.push(obj.ObjetivosEducativos.Unidad[i][j]));
+                        break;
+                    case 6:
+                        setRow6(row6.push(obj.ObjetivosEducativos.Unidad[i][j]));
+                        break;
+                    case 7:
+                        setRow7(row7.push(obj.ObjetivosEducativos.Unidad[i][j]));
+                        break;
+                }
+                
+            }
+        }
+        console.log(areas);
+        console.log(row1);
+        console.log(row5);
+        console.log(row6);
+        console.log(row7);
+    }, [])
+
+
     return(
         <>
-            <table className="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td colspan="2">Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                </tbody>
-            </table>
+        {/*
+            <div className="table table-success table-striped row">
+                <section className="col-2 d-inline-block">
+                    <div className="text-center row">{obj.AreaDeCrecimiento[0]}</div>
+                    {obj.ObjetivosEducativos.Unidad.Corporalidad.map((objetive, index) =>{
+                        return (
+                            <div key={"corp" + index} className="row">{objetive}</div>
+                        )
+                    })}
+                </section>
+                <section className="col-2 d-inline-block">
+                    <div className="text-center">{obj.AreaDeCrecimiento[1]}</div>
+                    {obj.ObjetivosEducativos.Unidad.Creatividad.map((objetive, index) =>{
+                        return (
+                            <div key={"crea" + index} className="">{objetive}</div>
+                        )
+                    })}
+                </section>
+                <section className="col-2 d-inline-block">
+                    <div className="text-center">{obj.AreaDeCrecimiento[2]}</div>
+                    {obj.ObjetivosEducativos.Unidad.Afectividad.map((objetive, index) =>{
+                        return (
+                            <div key={"afec" + index} className="">{objetive}</div>
+                        )
+                    })}
+                </section>
+                <section className="col-2 d-inline-block">
+                    <div className="text-center">{obj.AreaDeCrecimiento[3]}</div>
+                    {obj.ObjetivosEducativos.Unidad.Caracter.map((objetive, index) =>{
+                        return (
+                            <div key={"cara" + index} className="">{objetive}</div>
+                        )
+                    })}
+                </section>
+                <section className="col-2 d-inline-block">
+                    <div className="text-center">{obj.AreaDeCrecimiento[4]}</div>
+                    {obj.ObjetivosEducativos.Unidad.Sociabilidad.map((objetive, index) =>{
+                        return (
+                            <div key={"soc" + index} className="">{objetive}</div>
+                        )
+                    })}
+                </section>
+                <section className="col-2 d-inline-block">
+                    <div className="text-center">{obj.AreaDeCrecimiento[5]}</div>
+                    {obj.ObjetivosEducativos.Unidad.Corporalidad.map((objetive, index) =>{
+                        return (
+                            <div key={"Esp" + index} className="">{objetive}</div>
+                        )
+                    })}
+                </section>
+            </div>
+                */}
         </>
     )
 }
-
-module.exports( ObjAndAreas );
