2b87f6cb1473de2cdd045beecc3028225961ed0e
diegomaranges <diegomaranges@gmail.com>
2022-02-25 10:48:23 -0300

    fix some errors related with useEffect to load objetives in the table

--- src/Components/ObjAndAreas/BaseAreas/BaseAreas.jsx
+++ src/Components/ObjAndAreas/BaseAreas/BaseAreas.jsx
@@ -1,134 +1,128 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import reactDom from "react-dom";
+
 import * as afectividadU from "../../../sources/Unidad/AfectividadUnidad";
 import * as caracterU from "../../../sources/Unidad/CaracterUnidad";
 import * as corporalidadU from "../../../sources/Unidad/CorporalidadUnidad";
 import * as creatividadU from "../../../sources/Unidad/CreatividadUnidad";
 import * as sociabilidadU from "../../../sources/Unidad/SociabilidadUnidad";
 import * as areas from "../../../sources/Areas";
+import "./BaseAreas.css";
 
 export default function BaseAreas(params) {
-    const [allAreas, setAllAreas] = useState([])
-    const [afect, setAfect] = useState([]);
-    const [carac, setCarac] = useState([]);
-    const [corpo, setCorpo] = useState([]);
-    const [creat, setCreat] = useState([]);
-    const [socia, setSocia] = useState([]);
-    const [length, setlength] = useState(0);
-
-    function LoadSections() {
-        if (params.sec === "1") {
-        } else if (params.sec === "2") {
-            setAllAreas([]);
-            setAfect([]);
-            setCarac([]);
-            setCorpo([]);
-            setCreat([]);
-            setSocia([]);
-            setlength(0);
-            areas.areas.AreaDeCrecimiento.forEach((area, i) => {
-                if (i !== 5) {
-                    setAllAreas(...allAreas, area);
-                }
-            })
-            afectividadU.afecU.Afectividad.forEach(obj => {
-                setAfect([...afect, obj]);
-            });
-            caracterU.carU.Caracter.forEach(obj => {
-                setCarac([...carac, obj]);
-            });
-            corporalidadU.corpU.Corporalidad.forEach(obj => {
-                setCorpo([...corpo, obj]);
-            });
-            creatividadU.creatU.Creatividad.forEach(obj => {
-                setCreat([...creat, obj]);
-            });
-            sociabilidadU.socU.Sociabilidad.forEach(obj => {
-                setSocia([...socia, obj]);
-            });
-
-            setlength(Math.max(afect.length, carac.length, corpo.length, creat.length, socia.length));
-
-            if (afect.length < length) {
-                for (let i = afect.length; i <= length; i++) {
-                    setAfect(...afect, " ");
-                    console.log(afect);
-                }
-                for (let i = carac.length; i <= length; i++) {
-                    setCarac(...carac, " ");
-                }
-                for (let i = corpo.length; i <= length; i++) {
-                    setCorpo(...corpo, " ");
-                }
-                for (let i = corpo.length; i <= length; i++) {
-                    setCorpo(...creat, " ");
-                }
-                for (let i = socia.length; i <= length; i++) {
-                    setSocia(...socia, " ");
-                }
-            }
-        } else if (params.sec === "3") {
-        } else if (params.sec === "4") {
-        } else {
-            setAllAreas([]);
-            setAfect([]);
-            setCarac([]);
-            setCorpo([]);
-            setCreat([]);
-            setSocia([]);
-            setlength(0);
-        }
-    }
-
-    function loadAreasRow() {
-        return(
-            <tr>
-                <th scope="col">#</th>
-                {allAreas.map((area, i) => {
-                    return <th key={"areaO" + i} scope="col">{area}</th>
-                })}
-            </tr>
-        )
-    }
-
-    function loadDocRows() {
-        for (let i = 0; i < length; i++) {
-            return (
-                <tr>
-                    <th scope="row">{i + 1}</th>
-                    <th>{afect[i]}</th>
-                    <th>{carac[i]}</th>
-                    <th>{corpo[i]}</th>
-                    <th>{creat[i]}</th>
-                    <th>{socia[i]}</th>
-                </tr>
-            );
-        }
-    }
-
-    function loadTable () {
-        if (params.sec !== 0) {
-            LoadSections();
-            return (
-                <table className="table">
-                    <thead>
-                        {loadAreasRow()}
-                    </thead>
-                    <tbody>
-                        {loadDocRows()}
-                    </tbody>
-                </table>
-            )
-        }
-        return <div></div>
-    }
 
     useEffect(() => {
-        loadTable();
+        let allAreas = [];
+        let afect = [];
+        let carac = [];
+        let corpo = [];
+        let creat = [];
+        let socia = [];
+        let length = 0;
+
+        const LoadSections = () => {
+            if (params.sec === "1") {
+            } else if (params.sec === "2") {
+                areas.areas.AreaDeCrecimiento.forEach((area, i) => {
+                    if (i !== 5) {
+                        allAreas.push(area);
+                    }
+                })
+                afectividadU.afecU.Afectividad.forEach(obj => {
+                    afect.push(obj);
+                });
+                caracterU.carU.Caracter.forEach(obj => {
+                    carac.push(obj);
+                });
+                corporalidadU.corpU.Corporalidad.forEach(obj => {
+                    corpo.push(obj);
+                });
+                creatividadU.creatU.Creatividad.forEach(obj => {
+                    creat.push(obj);
+                });
+                sociabilidadU.socU.Sociabilidad.forEach(obj => {
+                    socia.push(obj);
+                });
+    
+                length = Math.max(afect.length, carac.length, corpo.length, creat.length, socia.length);
+    
+                if (afect.length < length) {
+                    for (let i = afect.length; i <= length; i++) {
+                        afect.push(" ");
+                    }
+                    for (let i = carac.length; i <= length; i++) {
+                        carac.push(" ");
+                    }
+                    for (let i = corpo.length; i <= length; i++) {
+                        corpo.push(" ");
+                    }
+                    for (let i = corpo.length; i <= length; i++) {
+                        creat.push(" ");
+                    }
+                    for (let i = socia.length; i <= length; i++) {
+                        socia.push(" ");
+                    }
+                }
+            } else if (params.sec === "3") {
+            } else if (params.sec === "4") {
+            } else {
+            }
+        }
+    
+        const loadAreasRow = () => {
+            return(
+                <tr>
+                    <th scope="col">#</th>
+                    {allAreas.map((area, i) => {
+                        return <th className="fit" key={"area" + i} scope="col">{area}</th>
+                    })}
+                </tr>
+            )
+        }
+    
+        const loadDocRows = () => {
+            let total = [];
+            total = [...Array(length).keys()]
+            return (
+                <>
+                    {total.map(e => {
+                            return <tr key={"tr" + e}>
+                                <th key={"number" + (e+1)} scope="row">{e + 1}</th>
+                                <th className="fit" key={"afect" + e}>{afect[e]}</th>
+                                <th className="fit" key={"carac" + e}>{carac[e]}</th>
+                                <th className="fit" key={"corpo" + e}>{corpo[e]}</th>
+                                <th className="fit" key={"creat" + e}>{creat[e]}</th>
+                                <th className="fit" key={"socia" + e}>{socia[e]}</th>
+                            </tr>
+                    })}
+                </>
+                );
+        }
+
+        const loadTable = () => {
+            if (params.sec !== 0) {
+                LoadSections();
+                return (
+                    <table className="table table-success table-striped">
+                        <thead>
+                            {loadAreasRow()}
+                        </thead>
+                        <tbody>
+                            {loadDocRows()}
+                        </tbody>
+                    </table>
+                )
+            } else {
+                return <div></div>
+            }
+        }
+    
+        return(reactDom.render(loadTable(), document.querySelector("#objetives")))
     }, [params.sec])
 
     return (
         <>
-            
+            <div id="objetives"></div>
         </>
     )    
 }
--- src/sources/Unidad/SociabilidadUnidad.js
+++ src/sources/Unidad/SociabilidadUnidad.js
@@ -3,7 +3,7 @@ export const socU = {
         "Promuevo el respeto por los Derechos Humanos y reconozco cuando son violados",
         "Reconozco las responsabilidades que implica el ejercicio de la autoridad y demuestro capacidad para compartir y ejecutar acuerdos",
         "Respeto las normas de convivencia de los distintos ambientes en que actúo",
-        "Presto un servicio individual cada día ( buena acción diaria )",
+        "Presto un servicio individual cada día (buena acción diaria)",
         "Valoro y participo en actividades que expresen la cultura de mi país",
         "Conozco la historia del Movimiento Scout. Me intereso por conocer las formas de vida de otros países",
         "Protejo la Naturaleza en mis actividades al aire libre"
